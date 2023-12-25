cb5222b0df1e0b3069be87349d420c04a015e2cd
Okan3465 <okan34.karakoc@gmail.com>
2023-12-26 01:47:39 +0300

    Sozluk

---
+++ src/Api/Core/BlazorSozluk.Api.Domain/Models/Entry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Domain.Models
+{
+    public  class Entry :BaseEntity
+    {
+        public string Subject { get; set; }
+
+        public string Content { get; set; }
+
+        public Guid CreatedById { get; set; }  //Entryi'i oluşturan kişinin ID'si                                            
+        public virtual User CreatedBy { get; set; } //yukarıdaki ID'den User'a ulaşabilmem lazım
+
+        public virtual ICollection<EntryComment> EntryComments { get; set; }  //Birden fazla yorum olacağı için ICollection kullanıyoruz.
+                                                                              //ilişkiler kurulurken virtual tanımlanmazsa include ederken hata alınır.
+                                                                              //Virtual olarak tanımlıyoruz kullanmadığımız zaman da hata almasın
+        public virtual ICollection<EntryVote> EntryVotes { get; set; }
+        public virtual ICollection<EntryFavorite> Favorites { get; set; }
+    }
+}
---
+++ src/Api/Core/BlazorSozluk.Api.Domain/Models/EntryComment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Domain.Models
+{
+    public class EntryComment :BaseEntity
+    {
+        public string Content { get; set; }
+
+        public Guid CreatedById { get; set; }
+        public Guid EntryId { get; set; }  //Commentin hangi entry'e ait olduğunuj gösterir
+        public virtual  Entry Entry { get; set; }
+        public virtual User CreatedBy { get; set; }
+        public virtual ICollection<EntryCommentVote> EntryCommentVotes { get; set; }
+        public virtual ICollection<EntryCommentFavorite> EntryCommentFavorites { get; set; }
+
+    }
+}
