82829344df1c30dd26f12d7c2f725c6aa3d79772
Okan3465 <okan34.karakoc@gmail.com>
2023-12-29 00:31:41 +0300

    29.12.2023
    Program.cs_Error

--- src/Api/WebApi/BlazorSozluk.Api.WebApi/Program.cs
+++ src/Api/WebApi/BlazorSozluk.Api.WebApi/Program.cs
@@ -1,3 +1,8 @@
+
+using BlazorSozluk.Infrastructure.Persistence.Extensions;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +12,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddInfrastructureRegistration();
+builder.Services.AddInfrastructureRegistration(builder.Configuration);
 
 var app = builder.Build();
 
