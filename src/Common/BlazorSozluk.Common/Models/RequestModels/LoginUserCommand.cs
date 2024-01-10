30b54be7013629da79d74f3c7fbe71c2d8953c80
Okan3465 <okan34.karakoc@gmail.com>
2024-01-10 23:47:23 +0300

    LoginUser
    10.01.2024

---
+++ src/Common/BlazorSozluk.Common/Models/RequestModels/LoginUserCommand.cs
@@ -0,0 +1,26 @@
+﻿using BlazorSozluk.Common.Models.Queries;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Common.Models.RequestModels
+{
+    public class LoginUserCommand : IRequest<LoginUserViewModel>  /*LoginUserViewModel  dışarı dönecek değerler*/
+    {
+        public string EmailAddress { get; set; }  //Dışarıdan alınacak değer
+        public string Password { get;set; }    //Dışarıdan alınacak değer
+
+        public LoginUserCommand(string emailAddress, string password)
+        {
+            EmailAddress = emailAddress;
+            Password = password;
+        }
+        public LoginUserCommand()
+        {
+
+        }
+    }
+}
